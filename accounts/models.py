56d3a6fe824fe4e801de7654927de3b87f065d54
Askhab Musaev <42musaev@gmail.com>
2021-04-10 21:04:31 +0300

    added custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    USERNAME_FIELD = 'username'
+    full_name = models.CharField(max_length=1024)
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
