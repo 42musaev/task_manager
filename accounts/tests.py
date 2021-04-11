5ef77a62c269b47a7c6fd013341d53d26014a5ef
Askhab Musaev <42musaev@gmail.com>
2021-04-11 14:25:54 +0300

    updated tests for Accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,14 +7,27 @@ User = get_user_model()
 
 
 class AccountsTest(APITestCase):
-    def test_register_account(self):
+
+    def setUp(self):
         url = reverse('account_register')
         data = {
             'username': 'test_user',
             'password': 'test_password',
             'full_name': 'test_full_name'
         }
-        response = self.client.post(url, data, follow='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.response = self.client.post(url, data, follow='json')
+        self.user = User.objects.get(username='test_user')
+
+    def test_register_account(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.get().username, 'test_user')
+
+    def test_login_user(self):
+        data = {
+            'username': 'test_user',
+            'password': 'test_password',
+        }
+        url = reverse('token_obtain_pair')
+        response = self.client.post(url, data, follow='json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
