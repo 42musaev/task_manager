fbc9f027d794ee65aabdf27430789156b94dfa1e
Askhab Musaev <42musaev@gmail.com>
2021-04-11 13:58:10 +0300

    added test AccountsTest

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+User = get_user_model()
+
+
+class AccountsTest(APITestCase):
+    def test_create_account(self):
+        url = reverse('account_register')
+        data = {
+            'username': 'test_user',
+            'password': 'test_password',
+            'full_name': 'test_full_name'
+        }
+        response = self.client.post(url, data, follow='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'test_user')
